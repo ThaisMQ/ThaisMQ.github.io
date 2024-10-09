d1526550ea232adc457174ef4c97a623a85a35ec
Thais <thais.quaresma@icen.ufpa.br>
2024-10-09 10:20:20 -0300

    Adicionando footer nas páginas de figma e pixel art. Aprimorando responsividade da página index

--- scripts/subpages.js
+++ scripts/subpages.js
@@ -1,14 +1,5 @@
-
-const caminhos = {
-    int_frame: "../pages/subpages/art01.html",
-  };
-  
-  const imagem = document.getElementById('image_click');
-  const frame_element = document.getElementById('frame_element');
-  const frame_container = document.getElementById('frame_container');
-  
-  imagem.addEventListener('click', function() {
-    frame_element.src = caminhos.int_frame; 
-    frame_container.style.display = 'block';
-  });
-  
+document.querySelectorAll('.container_draw').forEach(item => {
+    item.addEventListener('click', () => {
+        
+    });
+});
